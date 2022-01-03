dcfeff2c2c45ebd71f722f458eb6eb6936633a6e
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-03 04:34:26 -0600

    edit

--- app.js
+++ app.js
@@ -5,19 +5,18 @@ arrows.forEach((arrow, i) => {
   const itemNumber = movieLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
   arrow.addEventListener("click", () => {
-    const ratio = Math.floor(window.innerWidth / 270);
+    const ratio = Math.floor(window.innerWidth / 350);
     clickCounter++;
     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
-      }px)`;
+      movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+        }px)`;
     } else {
       movieLists[i].style.transform = "translateX(0)";
       clickCounter = 0;
     }
   });
 
-  console.log(Math.floor(window.innerWidth / 270));
+  console.log(Math.floor(window.innerWidth / 350));
 });
 
 //TOGGLE
